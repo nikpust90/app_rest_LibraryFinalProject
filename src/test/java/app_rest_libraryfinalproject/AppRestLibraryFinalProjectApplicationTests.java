0d31acc910a564b655506bae5f8a042df0358851
nikpust90 <nikolai.first21@yandex.ru>
2025-03-09 17:19:03 +0300

    Initial commit

---
+++ src/test/java/app_rest_libraryfinalproject/AppRestLibraryFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package app_rest_libraryfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRestLibraryFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
